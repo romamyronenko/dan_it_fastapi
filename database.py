b915486bdb1e99e916b129ef806aa041c3ae65a5
romamyronenko123 <eoma575@gmail.com>
2024-07-13 09:28:26 +0300

    fix

--- database.py
+++ database.py
@@ -3,13 +3,14 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_USER = os.getenv("MYSQL_USER", 'roma')
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", '123')
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "todo_db")
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'mysql_todolist')
+print(MYSQL_HOST)
 DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
